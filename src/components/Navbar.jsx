055cad6cae38d86aa03bb7aebd0d2812e1ee02bb
sharmapratham-96918 <prathams96918@gmail.com>
2025-08-04 14:50:07 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,6 @@ const Navbar = ()=>{
     return (
         <nav className="bg-emerald-800 md:bg-blue-500 text-white  font-bold  p-3  flex items-center justify-between ">
       <h1 className="text-2xl">Khata Book</h1>
-      <button className="border-2 bg-gray-700 text-white border-black p-2 cursor-pointer hover:bg-gray-500 transition delay delay-150 duration-150" >Change Theme</button>
     </nav>
 
     )
